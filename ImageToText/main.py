9bb8259e51eefeb7d50967f7ba7b388156d74f63
beyazıt uysal <66223190+masanbasa3k@users.noreply.github.com>
2022-11-14 21:37:05 +0300

    ImageToText
    Image to text python project

---
+++ ImageToText/main.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from pytesseract import pytesseract
+#Define path to tessaract.exe
+path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+#Define path to image
+photo = 'test.png'
+path_to_image = f"C:/VisualStudioCode/python_folder/picToText/images/{photo}"
+
+#Point tessaract_cmd to tessaract.exe
+pytesseract.tesseract_cmd = path_to_tesseract
+
+#Open image with PIL
+img = Image.open(path_to_image)
+
+text = pytesseract.image_to_string(img)
+print(text)
