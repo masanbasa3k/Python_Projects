dc41726308268c826f0bb5293aa5e2e61408556c
Beyazıt UYSAL <66223190+masanbasa3k@users.noreply.github.com>
2023-02-05 20:22:31 +0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,12 @@
 import random
 
+# Enter lenght
 password_lenght = int(input('Enter lentght of your password : '))
 
+# alphabet, numbers and symbols
 string = '''abcdrfghijklmnopqrtstuvwxyz0123456789ABCDRFGHIJKLMNOPQRTSTUVWXYZ!'+%&/()=*?-_<>'''
 
+# creating password with random
 password = ''.join(random.sample(string,password_lenght))
 print(password)
 
